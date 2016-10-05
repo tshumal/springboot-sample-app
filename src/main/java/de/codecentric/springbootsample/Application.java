0bec9e02ed036da8ed13578bc03322f7976cc0b7
Lingani Tshuma <Lingani.Tshuma@absa.co.za>
2016-10-05 16:33:19 +0200

    Added service discovery

--- src/main/java/de/codecentric/springbootsample/Application.java
+++ src/main/java/de/codecentric/springbootsample/Application.java
@@ -17,8 +17,10 @@ package de.codecentric.springbootsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
